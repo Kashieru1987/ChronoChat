93f76825586d9aa22522047b8b47f097cd55eb3a
Mahdi Chowdhury (Kashieru1987) <mahdichowdhury07@gmail.com>
2025-01-06 09:28:25 +0000

    Made the (not so) difficult decision to not use JavaFX
    - i hate css
    - time to built a xmpp chat app using a game engine

--- src/main/java/xyz/chronoziel/chronochat/GeneralConstants.java
+++ src/main/java/xyz/chronoziel/chronochat/GeneralConstants.java
@@ -5,7 +5,4 @@ public class GeneralConstants {
 	private GeneralConstants() {
 	}
 
-	public static final String ACCOUNT_INFO_DIRECTORY = "D:\\ChronoChat Secret Data\\";
-	public static final String ACCOUNT_INFO_FILENAME = "Username and Password.txt";
-
 }
